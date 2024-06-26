48ef3fc2c65b019d6d0c4a4f22b0ec65cf1ff4e8
doloresgarcia <doloresg382@gmail.com>
2024-06-26 12:26:11 +0200

    updaded gun script

--- data_generation/condor/run_sequence_CLD_train.sh
+++ data_generation/condor/run_sequence_CLD_train.sh
@@ -7,108 +7,47 @@ SEED=${4}
 OUTPUTDIR=${5}
 DIR=${6}
 IDX=${7}
+
+
 mkdir ${DIR}
 mkdir ${DIR}/${SEED}
 cd ${DIR}/${SEED}
 
 
-# cp -r ${HOMEDIR}/gun/gun.cpp .
-# cp -r ${HOMEDIR}/gun/compile_gun.x .
-# cp -r /afs/cern.ch/work/m/mgarciam/private/CLDConfig/CLDConfig/cld_steer.py .
-# cp -r /afs/cern.ch/work/m/mgarciam/private/CLDConfig/CLDConfig/CLDReconstruction1.py .
-# mkdir ${DIR}/${SEED}/PandoraSettingsCLD
-# cp /afs/cern.ch/work/m/mgarciam/private/CLDConfig/CLDConfig/PandoraSettingsCLD/* ./PandoraSettingsCLD/
+cp -r ${HOMEDIR}/gun/gun.cpp .
+cp -r ${HOMEDIR}/gun/CMakeLists.txt . 
+cp -r /afs/cern.ch/work/m/mgarciam/private/CLDConfig/CLDConfig/cld_steer.py .
+cp -r /afs/cern.ch/work/m/mgarciam/private/CLDConfig/CLDConfig/CLDReconstruction1.py .
+mkdir ${DIR}/${SEED}/PandoraSettingsCLD
+cp /afs/cern.ch/work/m/mgarciam/private/CLDConfig/CLDConfig/PandoraSettingsCLD/* ./PandoraSettingsCLD/
 cp -r ${HOMEDIR}/condor/make_pftree_clic_bindings.py .
 cp -r ${HOMEDIR}/condor/tree_tools.py .
-# cp -r ${HOMEDIR}/gun/${GUNCARD} .
-# echo ${HOMEDIR}/gun/${GUNCARD}
-# echo ${INPUTFILE}
+cp -r ${HOMEDIR}/gun/${GUNCARD} .
 
-#first recompile gun locally 
-echo "  "
-echo " ================================================================================ "
-echo "  "
-echo "compiling gun"
-echo "  "
-echo " ===============================================================================  "
-echo "  "
-
+# Load key4hep 
 wrapperfunction() {
     # source /cvmfs/sw.hsf.org/key4hep/setup.sh
     source /cvmfs/sw-nightlies.hsf.org/key4hep/setup.sh -r 2024-03-07
 }
 wrapperfunction
 
+# Build gun 
+mkdir build install
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=../install
+make install -j 8
+cd ..
+./build/gun ${GUNCARD} 
 
-# source compile_gun.x
+# Run simulation 
+ddsim --compactFile $K4GEO/FCCee/CLD/compact/CLD_o2_v05/CLD_o2_v05.xml --outputFile out_sim_edm4hep.root --steeringFile cld_steer.py --inputFiles events.hepmc --numberOfEvents ${NEV} --random.seed ${SEED}
 
+# Run reco 
+k4run CLDReconstruction1.py -n ${NEV}  --inputFile out_sim_edm4hep.root --outputBasename out_reco_edm4hep
 
-# echo "  "
-# echo " ================================================================================ "
-# echo "  "
-# echo "running gun"
-# echo "  "
-# echo " ===============================================================================  "
-# echo "  "
-# ./gun ${GUNCARD} 
-
-# echo "  "
-# echo " ================================================================================ "
-# echo "  "
-# echo "gun complete ..."
-# echo "  "
-# echo " ================================================================================ "
-# echo "  "
-
-
-# # # source /cvmfs/sw.hsf.org/key4hep/releases/2024-03-10/x86_64-almalinux9-gcc11.3.1-opt/key4hep-stack/2024-03-10-gidfme/setup.sh
-# # #When you want to revert the changes:
-
-
-# echo "  "
-# echo " ================================================================================ "
-# echo "  "
-# echo "run simulation ..."
-# echo "  "
-# echo " ================================================================================ "
-# echo "  "
-# ddsim --compactFile $K4GEO/FCCee/CLD/compact/CLD_o2_v05/CLD_o2_v05.xml --outputFile out_sim_edm4hep.root --steeringFile cld_steer.py --inputFiles events.hepmc --numberOfEvents ${NEV} --random.seed ${SEED}
-
-# echo "  "
-# echo " ================================================================================ "
-# echo "  "
-# echo "run reconstruction ..."
-# echo "  "
-# echo " ================================================================================ "
-# echo "  "
-
-# k4run CLDReconstruction1.py -n ${NEV}  --inputFiles out_sim_edm4hep.root --outputBasename out_reco_edm4hep
-
-echo "  "
-echo " ================================================================================ "
-echo "  "
-echo "produce pf tree ..."
-echo "  "
-echo " ================================================================================ "
-echo "  "
-
+# Create Tree 
 python make_pftree_clic_bindings.py out_reco_edm4hep_edm4hep.root tree.root False False ${IDX}
-
-echo "  "
-echo " ================================================================================ "
-echo "  "
-echo "copying output file  ..."
-echo "  "
-echo " ================================================================================ "
-echo "  "
 
 mkdir -p ${OUTPUTDIR}
 python /afs/cern.ch/work/f/fccsw/public/FCCutils/eoscopy.py tree.root ${OUTPUTDIR}/pf_tree_${SEED}.root
 
-echo "  "
-echo " ================================================================================ "
-echo "  "
-echo "file copied ... "
-echo "  "
-echo " ================================================================================ "
-echo "  "
--- data_generation/condor/script_create_dataset_train.sh
+++ data_generation/condor/script_create_dataset_train.sh
@@ -5,12 +5,13 @@
 
 # python submit_jobs_train.py --config config_pion0.gun --outdir /eos/experiment/fcc/ee/datasets/mlpf/CLD/train/single_particles_flat/pion0_0/ --condordir /eos/experiment/fcc/ee/datasets/mlpf/condor/train/single_particles_flat/pion0/ --idx 1 --njobs 1000 --nev 100 --queue workday
 
-python submit_jobs_train.py --config config_pion.gun --outdir /eos/experiment/fcc/ee/datasets/mlpf/CLD/train/single_particles_flat/pion_1/ --condordir /eos/experiment/fcc/ee/datasets/mlpf/condor/train/single_particles_flat/pion_1/ --idx 0 --njobs 4000 --nev 100 --queue workday
+# python submit_jobs_train.py --config config_pion.gun --outdir /eos/experiment/fcc/ee/datasets/mlpf/CLD/train/single_particles_flat/pion_v2/ --condordir /eos/experiment/fcc/ee/datasets/mlpf/condor/train/single_particles_flat/pion_v2/ --idx 0 --njobs 4000 --nev 100 --queue workday
 
 # python submit_jobs_train.py --config config_electron.gun --outdir /eos/experiment/fcc/ee/datasets/mlpf/CLD/train/single_particles_flat/electron_0/ --condordir /eos/experiment/fcc/ee/datasets/mlpf/condor/train/single_particles_flat/electron/ --idx 2 --njobs 1000 --nev 100 --queue workday
 
 # python submit_jobs_train.py --config config_muon.gun --outdir /eos/experiment/fcc/ee/datasets/mlpf/CLD/train/single_particles_flat/muon_0/ --condordir /eos/experiment/fcc/ee/datasets/mlpf/condor/train/single_particles_flat/muon/ --idx 3 --njobs 4000 --nev 100 --queue workday
 
-python submit_jobs_train.py --config config_resonance.gun --outdir /eos/experiment/fcc/ee/datasets/mlpf/CLD/train/single_particles_flat/rho_1/ --condordir /eos/experiment/fcc/ee/datasets/mlpf/condor/train/single_particles_flat/rho_1/ --idx 4 --njobs 4000 --nev 100 --queue workday
+# python submit_jobs_train.py --config config_resonance.gun --outdir /eos/experiment/fcc/ee/datasets/mlpf/CLD/train/single_particles_flat/rho_v2/ --condordir /eos/experiment/fcc/ee/datasets/mlpf/condor/train/single_particles_flat/rho_v2/ --idx 4 --njobs 4000 --nev 100 --queue workday
 
+python submit_jobs_train.py --config config_tau.gun --outdir /eos/experiment/fcc/ee/datasets/mlpf/CLD/train/single_particles_flat/tau/ --condordir /eos/experiment/fcc/ee/datasets/mlpf/condor/train/single_particles_flat/tau/ --idx 5 --njobs 4000 --nev 100 --queue workday
 
